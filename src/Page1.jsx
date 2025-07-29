0c5d87d86b117f613bf92b3dcd36376ff2114068
harikirank121 <harikirankaite@gmail.com>
2025-07-29 15:31:04 +0530

    first commit

---
+++ src/Page1.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+export default class Page1 extends Component {
+  render() {
+    return (
+      <div className='main2'>
+        <div className='hi'>
+        <h1 >Hello World</h1>
+        </div>
+        <h2>the frontend </h2>
+      </div>
+    )
+  }
+}
